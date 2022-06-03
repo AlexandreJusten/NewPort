b8dffe0472f53a91d5efecb16d2ec71796942d14
Alexandre Justen Filho <58986915+AlexandreJusten@users.noreply.github.com>
2022-06-03 01:31:09 -0300

    Delete src directory

--- src/main/java/com/spring/blogalex/repository/BlogAlexRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.blogalex.repository;
-
-import com.spring.blogalex.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogAlexRepository extends JpaRepository<Post,Long> {
-}
